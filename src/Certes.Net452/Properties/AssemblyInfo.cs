b7412378ccebf62f2ef7d959e6c7058e41bc7402
Arley Schrock <arleyschrock@outlook.com>
2017-03-27 12:37:04 -0500

    add package.nuspec

--- src/Certes.Net452/Properties/AssemblyInfo.cs
+++ src/Certes.Net452/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Certes.Net452")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Certes.Net452")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Certes")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,17 +16,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0580349c-c8bc-4449-af25-6ec2afbbf4e1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Guid("5f6267a0-a6c5-4d1e-b7ff-f8f8d09d8588")]
+//[assembly: InternalsVisibleTo("Certes.Tests")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyTitle("Certe")]
+[assembly: AssemblyDescription("letsencyrpt.org automation client")]
+[assembly: AssemblyCopyright("Copyright @ 2017")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
